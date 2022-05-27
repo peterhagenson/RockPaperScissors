12e741f425da3d3f05bbb3ebf5ba83052c00a3fc
Peter Hagenson <p.hagenson@gmail.com>
2022-05-27 08:40:29 -0500

    Add JS.js

--- JS.js
+++ JS.js
@@ -7,16 +7,32 @@ function computerPlay() {
   return random;
 }
 
-function playRound(playerSelction, computerSelection) {
-  if (computerSelection === "Paper") {
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "Rock" && computerSelection === "Paper") {
     return `You lose! ${computerSelection} beats ${playerSelection}!`;
-  } else if (computerSelection === "Scissors") {
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
     return `You win! ${playerSelection} beats ${computerSelection}!`;
+  } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    return `You lose! ${computerSelection} beats ${playerSelection}!`;
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return `You win! ${playerSelection} beats ${computerSelection}!`;
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return `You win! ${playerSelection} beats ${computerSelection}!`;
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    return `You lose! ${computerSelection} beats ${playerSelection}!`;
   } else if (playerSelection === computerSelection) {
     return "It's a tie!";
   }
 }
 
-const playerSelection = "Rock";
+let playerSelection = window.prompt("Choose Rock, Paper, or Scissors");
 const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+  for (let i = 1; i <= 5; i++) {
+    let playerSelection = window.prompt("Choose Rock, Paper, or Scissors");
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+}
+console.log(game());
